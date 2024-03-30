9ceab3acd870f1434778aaf12457ac72c20696f7
henriqueftcunha@gmail.com <henriqueftcunha@gmail.com>
2024-03-30 00:02:40 -0300

    Mudei o código das funções no arquivo std.py do módulo 6

--- M6/std.py
+++ M6/std.py
@@ -1,43 +1,23 @@
-def getInt(input_str):
-    Num = []
-    for i in range(10):
-        Num.append(f"{i}")
+def getInt(input_str: str) -> int:
+
     while True:
-        number = input(input_str)
-        count = 0
-        sign = 0
-        for i in range(len(number)):
-            if number[i] in Num:
-                count += 1
-            elif number[0] == '-' and sign < 1:
-                sign += 1
-            else:
-                break
-        if len(number) == count + sign and len(number) > 0:
-            return int(number)
-        else:
+
+        numStr = input(input_str)
+
+        try:
+            num = int(numStr)
+            return num
+        except:
             print("That's not an integer")
 
-def getFloat(input_str):
-    Num = []
-    for i in range(10):
-        Num.append(f'{i}')
-    while True:
-        num = input(input_str)
-        count = 0
-        point = 0
-        sign = 0
-        for digit in num:
-            if digit in Num:
-                count += 1
-            elif digit == '.' and point < 1:
-                point += 1
-            elif digit == '-' and sign < 1:
-                sign += 1
-            else:
-                break
-        if len(num) == count + point + sign and len(num) != 0:
-            return float(num)
-        else:
-            print("That's not a float")
+def getFloat(input_str: str) -> float:
 
+    while True:
+
+        numStr = input(input_str)
+
+        try:
+            num = float(numStr)
+            return num
+        except:
+            print("That's not a float")
